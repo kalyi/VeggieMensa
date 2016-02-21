928a4d00ae39d40235b1db3a0c5081e642328afd
Olaf Lessenich <xai@linux.com>
2016-02-22 00:16:55 +0100

    Reformat code to comply with PEP8
    Signed-off-by: Olaf Lessenich <xai@linux.com>

--- src/veggieMensa.py
+++ src/veggieMensa.py
@@ -22,30 +22,33 @@
 
 """Show vegetarian/vegan meals of STWNO canteens."""
 
-################################################################################
+###############################################################################
 
-import csv, urllib.request, datetime, argparse
+import csv
+import urllib.request
+import datetime
+import argparse
 
-################################################################################
+###############################################################################
 
 stwnoUrl = "http://www.stwno.de/infomax/daten-extern/csv/{}/{}.csv"
 
 canteens = {
-    'pa': ('UNI-P','Mensa Uni Passau'),
+    'pa': ('UNI-P', 'Mensa Uni Passau'),
     'pa-nk': ('Cafeteria Nikolakloster', 'Uni Passau Cafeteria Nikolakloster'),
     'rgbg': ('UNI-R', 'Mensa Uni Regensburg (mittags)')
 }
 
 markedIngredients = {
-    '1' : 'mit Farbstoff',
-    '2' : 'mit Konservierungsstoff',
-    '3' : 'mit Antioxidationsmittel',
-    '4' : 'mit Geschmacksverstärker',
-    '5' : 'geschwefelt',
-    '6' : 'geschwärzt',
-    '7' : 'gewachst',
-    '8' : 'mit Phosphat',
-    '9' : 'mit Süßungsmittel Saccharin',
+    '1': 'mit Farbstoff',
+    '2': 'mit Konservierungsstoff',
+    '3': 'mit Antioxidationsmittel',
+    '4': 'mit Geschmacksverstärker',
+    '5': 'geschwefelt',
+    '6': 'geschwärzt',
+    '7': 'gewachst',
+    '8': 'mit Phosphat',
+    '9': 'mit Süßungsmittel Saccharin',
     '10': 'mit Süßungsmittel Aspartam, enthält Phenylalaninquelle',
     '11': 'mit Süßungsmittel Cyclamat',
     '12': 'mit Süßungsmittel Acesulfam',
@@ -57,33 +60,34 @@ markedIngredients = {
 }
 
 allergens = {
-    'A' : 'Gluten',
-    'B' : 'Krebstiere',
-    'C' : 'Eier',
-    'D' : 'Fisch',
-    'E' : 'Erdnüsse',
-    'F' : 'Soja',
-    'G' : 'Milch und Milchprodukte',
-    'H' : 'Schalenfrüchte',
-    'I' : 'Sellerie',
-    'J' : 'Senf',
-    'K' : 'Sesamsamen',
-    'L' : 'Schwefeldioxid und Sulfite',
-    'M' : 'Lupinen',
-    'N' : 'Weichtiere'
+    'A': 'Gluten',
+    'B': 'Krebstiere',
+    'C': 'Eier',
+    'D': 'Fisch',
+    'E': 'Erdnüsse',
+    'F': 'Soja',
+    'G': 'Milch und Milchprodukte',
+    'H': 'Schalenfrüchte',
+    'I': 'Sellerie',
+    'J': 'Senf',
+    'K': 'Sesamsamen',
+    'L': 'Schwefeldioxid und Sulfite',
+    'M': 'Lupinen',
+    'N': 'Weichtiere'
 }
 
-################################################################################
+
+###############################################################################
 class Dish:
     def __init__(self, name, category, tags, prices):
         self.category = category
         self.tags = tags.split(',')
         self.prices = prices
-        self.markedIngredients = [ ]
-        self.allergens = [ ]
+        self.markedIngredients = []
+        self.allergens = []
         self.parseName(name)
 
-    def parseName(self,name):
+    def parseName(self, name):
         name = name.strip()
         if name.endswith(')'):
             i = name.rfind('(')
@@ -98,7 +102,7 @@ class Dish:
         items = extra.split(',')
         for i in items:
             if i.isdigit():
-               self.markedIngredients.append(i)
+                self.markedIngredients.append(i)
             elif i.isalpha() and len(i) == 1:
                 self.allergens.append(i)
 
@@ -123,25 +127,25 @@ class Dish:
     def isOrganic(self):
         return 'B' in self.tags or 'bio' in self.name.lower()
 
-    def formatMarkedIngredients(self,select=None):
+    def formatMarkedIngredients(self, select=None):
         if len(self.markedIngredients) == 0:
             return ''
-        ing = ([ markedIngredients[i] for i in self.markedIngredients]
-                if select is None else
-                [ markedIngredients[i] for i in self.markedIngredients
-                    if i in select ])
+        ing = ([markedIngredients[i] for i in self.markedIngredients]
+               if select is None else
+               [markedIngredients[i] for i in self.markedIngredients
+                if i in select])
         return '[{}]'.format(', '.join(ing)) if len(ing) > 0 else ''
 
-    def formatAllergens(self,select=None):
+    def formatAllergens(self, select=None):
         if len(self.allergens) == 0:
             return ''
-        allerg = ([ allergens[i] for i in self.allergens ] if select is None
-                else [ allergens[i] for i in self.allergens if i in select ])
+        allerg = ([allergens[i] for i in self.allergens] if select is None
+                  else [allergens[i] for i in self.allergens if i in select])
         return '[{}]'.format(', '.join(allerg)) if len(allerg) > 0 else ''
 
-    def formatPrice(self,select=(True,True,True)):
+    def formatPrice(self, select=(True, True, True)):
         return ('EUR {}'.format('/'.join(
-            [ p for (p,b) in zip(self.prices,select) if b ] ))
+            [p for (p, b) in zip(self.prices, select) if b]))
             if True in select else '')
 
     def prettyPrint(self, ingredients, allergens, prices):
@@ -149,8 +153,8 @@ class Dish:
         allerg = self.formatAllergens(allergens)
         price = self.formatPrice(prices)
         s = ("{} {}{}".format(self.name, mIng, allerg)
-                if len(mIng) + len(allerg) > 0 else self.name)
-        return '{}: {}'.format(s,price) if len(price) > 0 else s
+             if len(mIng) + len(allerg) > 0 else self.name)
+        return '{}: {}'.format(s, price) if len(price) > 0 else s
 
     def __str__(self):
         mIng = self.formatMarkedIngredients()
@@ -160,9 +164,9 @@ class Dish:
                     self.name, mIng, allerg, self.formatPrice())
         return "{} : {}".format(self.name, self.formatPrice())
 
-################################################################################
 
-def getCSV(canteen='pa',week=0):
+###############################################################################
+def getCSV(canteen='pa', week=0):
     if week < 1:
         week = datetime.date.today().isocalendar()[1]
     url = stwnoUrl.format(canteens[canteen][0], week)
@@ -171,25 +175,27 @@ def getCSV(canteen='pa',week=0):
         text = data.decode('latin1')
     return text.split('\n')
 
+
 def parseCSV(lines):
     spamreader = csv.reader(lines[1:], delimiter=';')
-    menu = { }
+    menu = {}
     for row in spamreader:
-        if len(row)  < 5:
+        if len(row) < 5:
             continue
-        date = datetime.datetime.strptime(row[0],'%d.%m.%Y')
-        dish = Dish(row[3],row[2],row[4],(row[6],row[7],row[8]))
+        date = datetime.datetime.strptime(row[0], '%d.%m.%Y')
+        dish = Dish(row[3], row[2], row[4], (row[6], row[7], row[8]))
         if date in menu.keys():
             menu[date].append(dish)
         else:
-            menu[date] = [ dish ]
+            menu[date] = [dish]
     return menu
 
+
 def prettyPrintDay(menu, day, checkVegetarian, checkVegan, checkOrganic,
-        markedIngredients, allergens, prices):
+                   markedIngredients, allergens, prices):
     print('*** {} ***'.format(datetime.datetime.strftime(day, '%A, %d.%m.%Y')))
-    byCategory = { 'soup' : [], 'main' : [], 'side': [],
-            'dessert' : [], 'unknown': [] }
+    byCategory = {'soup': [], 'main': [], 'side': [],
+                  'dessert': [], 'unknown': []}
     for dish in menu[day]:
         if (checkVegetarian and not dish.isVegetarian()) or (
                 checkVegan and not dish.isVegan()) or (
@@ -205,32 +211,34 @@ def prettyPrintDay(menu, day, checkVegetarian, checkVegan, checkOrganic,
             byCategory['dessert'].append(dish)
         else:
             byCategory['unknown'].append(dish)
-    categories = [ ('Suppen', byCategory['soup']),
-            ('Hauptgerichte',byCategory['main']),
-            ('Beilagen',byCategory['side']),
-            ('Nachspeisen',byCategory['dessert']),
-            ('Sonstiges',byCategory['unknown']) ]
+    categories = [('Suppen', byCategory['soup']),
+                  ('Hauptgerichte', byCategory['main']),
+                  ('Beilagen', byCategory['side']),
+                  ('Nachspeisen', byCategory['dessert']),
+                  ('Sonstiges', byCategory['unknown'])]
     for n, dishes in categories:
         if len(dishes) > 0:
             print('*\n* {}:'.format(n))
             for dish in dishes:
                 print('*   {}'.format(dish.prettyPrint(markedIngredients,
-                    allergens, prices)))
+                                                       allergens, prices)))
     print('*\n**********\n')
 
+
 def prettyPrint(menu, day, checkVegetarian, checkVegan, checkOrganic,
-        markedIngredients, allergens, prices):
+                markedIngredients, allergens, prices):
     days = sorted(menu.keys())
     if day is None:
         for day in days:
             prettyPrintDay(menu, day, checkVegetarian, checkVegan,
-                    checkOrganic, markedIngredients, allergens, prices)
+                           checkOrganic, markedIngredients, allergens, prices)
     else:
         if day in range(len(days)):
             prettyPrintDay(menu, days[day], checkVegetarian, checkVegan,
-                    checkOrganic, markedIngredients, allergens, prices)
+                           checkOrganic, markedIngredients, allergens, prices)
         else:
             print("No menu for this day.")
+
 
 def parseDay(day):
     week = datetime.date.today().isocalendar()[1]
@@ -240,7 +248,7 @@ def parseDay(day):
         d = day.lower()
         today = datetime.date.today().weekday()
         if d == 'today':
-            return (today,week)
+            return (today, week)
         elif d == 'tomorrow':
             tomorrow = today + 1
             return (tomorrow % 7, week + tomorrow / 7)
@@ -249,57 +257,63 @@ def parseDay(day):
         elif d == 'nextweek':
             return(None, week + 1)
         else:
-            mapDays = zip(['mon','tue','wed','thu','fri','sat','sun'],
-                    range(0,7))
+            mapDays = zip(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+                          range(0, 7))
             for s, n in mapDays:
                 if d.startswith(s):
                     return (n, week if n >= today else week + 1)
             return (None, week)
 
+
 def parseFilter(selected, options):
     if 'all' in selected:
         return list(options.keys())
-    foundOptions = [ ]
+    foundOptions = []
     for s in selected:
         for k, v in options.items():
             if s.lower() in v.lower() and k not in foundOptions:
                 foundOptions.append(k)
     return foundOptions
 
+
 def main():
     parser = argparse.ArgumentParser(
             description='Retrieves and filters menus of STWNO canteens.')
     parser.add_argument('-c', '--canteen', action='store',
-            choices=list(canteens.keys()), default='pa',
-            help='Select canteen. Type {}. \
-                    Defaults to Mensa Uni Passau.'.format(
-                ', '.join(['{} for {}'.format(
-                    s,canteens[s][1]) for s in canteens.keys()])))
+                        choices=list(canteens.keys()), default='pa',
+                        help='Select canteen. Type {}. \
+                        Defaults to Mensa Uni Passau.'.format(
+                            ', '.join(['{} for {}'.format(
+                                s, canteens[s][1]) for s in canteens.keys()])))
     parser.add_argument('-a', '--all', action='store_true', default=False,
-            help='Do not filter non-vegetarian dishes.')
+                        help='Do not filter non-vegetarian dishes.')
     parser.add_argument('-v', '--vegan', action='store_true', default=False,
-            help='Show only vegan dishes.')
+                        help='Show only vegan dishes.')
     parser.add_argument('-o', '--organic', action='store_true', default=False,
-            help='Show only organic dishes.')
-    parser.add_argument('-m', '--marked', action='append', default=[ ],
-            help='Show specified marked ingredient. Use \'all\' to show all.')
-    parser.add_argument('-l', '--allergen', action='append', default=[ ],
-            help='Show specified allergen. Use \'all\' to show all.')
+                        help='Show only organic dishes.')
+    parser.add_argument('-m', '--marked', action='append', default=[],
+                        help='Show specified marked ingredient. ' +
+                        'Use \'all\' to show all.')
+    parser.add_argument('-l', '--allergen', action='append', default=[],
+                        help='Show specified allergen. ' +
+                        'Use \'all\' to show all.')
     parser.add_argument('-w', '--week', action='store', type=int,
-            help='Show menu for specified week. Defaults to current week.')
+                        help='Show menu for specified week. ' +
+                        'Defaults to current week.')
     parser.add_argument('-s', '--student', action='store_true', default=False,
-            help='Show student prices.')
+                        help='Show student prices.')
     parser.add_argument('-e', '--employee', action='store_true', default=False,
-            help='Show employee prices.')
+                        help='Show employee prices.')
     parser.add_argument('-g', '--guest', action='store_true', default=False,
-            help='Show guest prices.')
+                        help='Show guest prices.')
     parser.add_argument('day', nargs='?',
-            choices=[ 'mon','tue','wed','thu','fri',
-                'today','tomorrow','curweek','nextweek' ],
-            help='Show menu only for specified day(s). Defaults to curweek.')
+                        choices=['mon', 'tue', 'wed', 'thu', 'fri',
+                                 'today', 'tomorrow', 'curweek', 'nextweek'],
+                        help='Show menu only for specified day(s). ' +
+                        'Defaults to curweek.')
     args = parser.parse_args()
     #
-    day,week = parseDay(args.day)
+    day, week = parseDay(args.day)
     if args.week is not None:
         week = args.week
     showIngredients = parseFilter(args.marked, markedIngredients)
@@ -307,18 +321,18 @@ def main():
     prices = (args.student, args.employee, args.guest)
     #
     print("\n{} menu for week {} at {}".format(
-        'Vegan' if args.vegan else 'Vegetarian' if not args.all else 'Complete',
-        week, canteens[args.canteen][1]))
+        'Vegan' if args.vegan else 'Vegetarian'
+        if not args.all else 'Complete', week, canteens[args.canteen][1]))
     if args.organic:
         print('Showing only organic dishes.')
     if True in prices:
         print('Prices are for {}.'.format('/'.join(
-            [ x for (x,b) in zip(['students','employees','guests'], prices)
-                if b ])))
+            [x for (x, b) in zip(['students', 'employees', 'guests'], prices)
+                if b])))
     print()
     prettyPrint(parseCSV(getCSV(args.canteen, week)), day,
-        not args.all, args.vegan, args.organic,
-        showIngredients, showAllergens, prices)
+                not args.all, args.vegan, args.organic,
+                showIngredients, showAllergens, prices)
 
 if __name__ == "__main__":
     main()
